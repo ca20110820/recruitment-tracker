6a67225b3247909558e65db8d61332a710c1e154
ca20110820 <ca20110820@gmail.com>
2023-09-17 11:45:36 +0800

    Initial implementation of Job class

--- TrackerApp/TrackerApp/Job.cs
+++ TrackerApp/TrackerApp/Job.cs
@@ -8,5 +8,35 @@ namespace TrackerApp
 {
     public class Job
     {
+
+        private Contractor? contractor = null; // Default to null
+
+        public string Title { get; private set; }
+        public DateOnly Date { get; set; }
+        public double Cost { get; set; }
+        public bool Completed { get; set; }
+
+        public Contractor? ContractorAssigned { get {  return contractor; } }
+
+        public Job(string title, string date, double cost, Contractor contractor)
+        {
+            this.Title = title;
+            this.Date = DateOnly.Parse(date);
+            this.Cost = cost;
+            this.contractor = contractor;
+        }
+        public Job(string title, string date, double cost)
+        {
+            // Assume: No Contractor given, null by default from backing field.
+            this.Title = title;
+            this.Date = DateOnly.Parse(date);
+            this.Cost = cost;
+        }
+
+        public void AssignContractor(Contractor contractor)
+        {
+            /* Assign a Contractor to the Job */
+            this.contractor = contractor;
+        }
     }
 }
