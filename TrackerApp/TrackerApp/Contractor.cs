a629fdce1c5c0dc4d2be2fdec8fe2230664ecf6b
ca20110820 <ca20110820@gmail.com>
2023-09-17 11:21:30 +0800

    Added constructors to Contractor class

--- TrackerApp/TrackerApp/Contractor.cs
+++ TrackerApp/TrackerApp/Contractor.cs
@@ -13,5 +13,42 @@ namespace TrackerApp
         public DateOnly StartDate { get; set; }
         public double HourlyWage { get; set; }
         public bool IsAvailable { get; set; }
+
+        public Contractor(string FirstName, string LastName, string StartDate, double HourlyWage, bool IsAvailable)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.StartDate = DateOnly.Parse(StartDate); // Assume StartDate argument is string
+            this.HourlyWage = HourlyWage;
+            this.IsAvailable = IsAvailable; // No Assumption
+        }
+        public Contractor(string FirstName, string LastName, double HourlyWage, bool IsAvailable)
+        {
+            // Assume: User dont include StartDate, use date now.
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.StartDate = new DateOnly(DateTime.Now.Date.Year, DateTime.Now.Date.Month, DateTime.Now.Date.Day); // Date Now
+            this.HourlyWage = HourlyWage;
+            this.IsAvailable = IsAvailable; // No Assumption
+        }
+        public Contractor(string FirstName, string LastName, string StartDate, double HourlyWage)
+        {
+            // Assume: Not Available by default
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.StartDate = DateOnly.Parse(StartDate); // Assume StartDate argument is string
+            this.HourlyWage = HourlyWage;
+            this.IsAvailable = false;
+        }
+        public Contractor(string FirstName, string LastName, double HourlyWage)
+        {
+            // Assume: User dont include StartDate, use date now; and Not Available by default
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.StartDate = new DateOnly(DateTime.Now.Date.Year, DateTime.Now.Date.Month, DateTime.Now.Date.Day); // Date Now
+            this.HourlyWage = HourlyWage;
+            this.IsAvailable = false;
+        }
+
     }
 }
