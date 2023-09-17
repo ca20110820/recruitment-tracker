e2aaab9015031cb98b09ab595b44f50cff981287
ca20110820 <ca20110820@gmail.com>
2023-09-17 12:00:20 +0800

    Initial implementation of RecruitmentSystem class with AddContractor method

--- TrackerApp/TrackerApp/RecruitmentSystem.cs
+++ TrackerApp/TrackerApp/RecruitmentSystem.cs
@@ -8,6 +8,23 @@ namespace TrackerApp
 {
     public class RecruitmentSystem
     {
+        private List<Contractor> contractors = new List<Contractor>();
+        private List<Job> jobs = new List<Job>();
+
+        public List<Contractor> Contractors { get {  return contractors; } }
+        public List<Job> Jobs { get {  return jobs; } }
+
+        public void AddContractor(Contractor newContractor)
+        {
+            // Check if contractor exist in contractors
+            bool contractorExists = contractors.Any(contractor => contractor.FirstName == newContractor.FirstName && contractor.LastName == newContractor.LastName);
+
+            // Add the New Contractor if they do not exists in our list of contractors
+            if (!contractorExists)
+            {
+                contractors.Add(newContractor);
+            }
+        }
 
     }
 }
